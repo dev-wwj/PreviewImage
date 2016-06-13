42aab79ab770121ae035c6b77248e569c2570d7c
fydd <fyddwwj@hotmail.com>
2016-06-13 11:57:42 +0800

    first commit

---
+++ ImagePreviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePreviewDemo
+//
+//  Created by 王文建 on 16/6/13.
+//  Copyright © 2016年 Scorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePreviewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePreviewDemo
+//
+//  Created by 王文建 on 16/6/13.
+//  Copyright © 2016年 Scorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
